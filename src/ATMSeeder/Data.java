4f2484cef44032075e0bdc5192a891b11434d462
Angel Ponce <acdpf000@gmail.com>
2021-08-29 19:05:57 -0600

    Adding random data 📊

---
+++ src/ATMSeeder/Data.java
@@ -0,0 +1,15 @@
+package ATMSeeder;
+
+/**
+ *
+ * @author Angel Ponce
+ */
+public class Data {
+
+    public static final String[] NAME_MAN = {"Reegan", "Emre", "Tayla", "Mikail", "Julius", "Dominick", "Nathanael", "Marek", "Diesel", "Rohit", "Campbell", "Hasnain", "Randall", "Devante", "Caine", "Aaryan", "Bobbi", "Erica", "Kenny", "Zaydan", "Ezekiel", "Marco", "Gilbert", "Cally", "Romy", "Haiden", "Montague", "Eli", "Ted", "Eduard"};
+    public static final String[] LASTNAME_MAN = {"Kelly", "Serrano", "Barrera", "Atkinson", "Boyle", "Valencia", "Stokes", "Fellows", "Palmer", "Stone", "Read", "Barnard", "Cote", "Whitfield", "Butt", "Wheeler", "Braun", "David", "Wooten", "Deleon", "Gaines", "Irving", "Odonnell", "Mcnamara", "Singleton", "Salter", "Owen", "Beech", "Stubbs", "Paine"};
+    public static final String[] NAME_WOMAN = {"Brodie", "Tahmina", "Vienna", "Ella-May", "Romana", "Ariana", "Farah", "Natalie", "Valentina", "Aurelia", "Harlow", "Hawa", "Jesse", "Ana", "Sia", "Hazel", "Chelsey", "Izzie", "Malia", "Hajra", "Eliot", "Rachelle", "Taryn", "Angharad", "Patrycja", "Viktoria", "Kelis", "Sharmin", "Komal", "Ellenor"};
+    public static final String[] LASTNAME_WOMAN = {"Glenn", "Gordon", "Martin", "Villa", "Sargent", "O'Ryan", "Becker", "Bloom", "Seymour", "Burris", "Cantrell", "Mill", "Holder", "Ryan", "Hodson", "Galloway", "Maddox", "Dodson", "Kumar", "Davila", "Gray", "Wiley", "Power", "Felix", "Cresswell", "Davenport", "Welch", "Salas", "John", "Blanchard"};
+    public static final String[] PICK_MAN = {"002-man.png", "005-man.png", "011-man.png", "013-man.png", "014-man.png", "019-man.png", "020-man.png", "021-man.png", "024-man.png", "026-man.png", "029-man.png", "032-man.png", "034-man.png", "036-man.png", "038-man.png", "041-man.png", "049-man.png"};
+    public static final String[] PICK_WOMAN = {"001-woman.png", "003-woman.png", "004-woman.png", "006-woman.png", "007-woman.png", "008-woman.png", "009-woman.png", "010-woman.png", "012-woman.png", "015-woman.png", "016-woman.png", "017-woman.png", "018-woman.png", "022-woman.png", "023-woman.png", "025-woman.png", "027-woman.png", "028-woman.png", "030-woman.png", "031-woman.png", "033-woman.png", "035-woman.png", "037-woman.png", "039-woman.png", "040-woman.png", "042-woman.png", "043-woman.png", "044-woman.png", "045-woman.png", "046-woman.png", "047-woman.png", "048-woman.png", "050-woman.png"};
+}
