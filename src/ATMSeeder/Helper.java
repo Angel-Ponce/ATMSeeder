a39e6c2bcfe8a06f8aa402c4838e8687bfe32ac3
Angel Ponce <acdpf000@gmail.com>
2021-08-29 20:35:16 -0600

    Create a random users 🎲

--- src/ATMSeeder/Helper.java
+++ src/ATMSeeder/Helper.java
@@ -9,12 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /**
--- src/ATMSeeder/Seed.java
+++ src/ATMSeeder/Seed.java
@@ -5,6 +5,7 @@ import Entity.Properties;
 import Entity.Admin;
 import Entity.Ticket;
 import Entity.User;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
@@ -29,6 +30,13 @@ public class Seed {
 
     public static void generate(int size) {
 
+        //Remove all files in database/profiles dir
+        File profiles = new File("database/profiles/");
+        for (File child : profiles.listFiles()) {
+            if (!child.isDirectory()) {
+                child.delete();
+            }
+        }
         //Create an ArrayList of Persons
         ArrayList<Person> persons = new ArrayList<>();
         //First generate a unique admin
@@ -43,6 +51,33 @@ public class Seed {
         Admin admin = new Admin(adminName, adminLastName, adminAge, adminEmail, adminPin, adminLastAccess, "database/profiles/" + adminPick);
         persons.add(admin);
 
+        for (int i = 1; i <= size; i++) {
+            int cardNumber = i;
+            int currentBalance = r.nextInt(10000);
+            int maximumAmount = r.nextInt(10000);
+            String sex = Data.SEX[r.nextInt(Data.SEX.length)];
+            String name = "user";
+            String lastName = "" + i;
+            String pick = Data.PICK_MAN[r.nextInt(Data.PICK_MAN.length)];
+            if (sex.equals(Data.MAN)) {
+                name = Data.NAME_MAN[r.nextInt(Data.NAME_MAN.length)];
+                lastName = Data.LASTNAME_MAN[r.nextInt(Data.LASTNAME_MAN.length)];
+                pick = Data.PICK_MAN[r.nextInt(Data.PICK_MAN.length)];
+            } else if (sex.equals(Data.WOMAN)) {
+                name = Data.NAME_WOMAN[r.nextInt(Data.NAME_WOMAN.length)];
+                lastName = Data.LASTNAME_WOMAN[r.nextInt(Data.LASTNAME_WOMAN.length)];
+                pick = Data.PICK_WOMAN[r.nextInt(Data.PICK_WOMAN.length)];
+            }
+            int age = r.nextInt(75);
+            String email = cardNumber + "";
+            int pin = cardNumber;
+            Date lastAccess = new Date();
+
+            User user = new User(cardNumber, currentBalance, maximumAmount, name, lastName, age, email, pin, lastAccess, "database/profiles/" + pick);
+            Helper.moveFile("/Resources/" + sex + "/" + pick, "database/profiles/" + pick);
+            persons.add(user);
+        }
+
         ArrayList<Ticket> tickets = new ArrayList();
         tickets.add(new Ticket(1, 10));
         tickets.add(new Ticket(5, 10));
@@ -52,15 +87,27 @@ public class Seed {
         tickets.add(new Ticket(100, 10));
         tickets.add(new Ticket(200, 10));
 
-        Properties properties = new Properties("", 3860, persons.get(0));
+        Properties properties = new Properties("", 3860, persons.get(1));
 
         if (Helper.saveObjectToFile(persons, "database/Persons.txt")
                 && Helper.saveObjectToFile(tickets, "database/Tickets.txt")
                 && Helper.saveObjectToFile(properties, "database/Properties.txt")) {
-            System.out.println("Seeder run successfuly");
-            System.out.println("*----- CREDENTIALS -----*");
+
+            System.out.println("");
+            System.out.println("*---------------------------*");
+            System.out.println("\tSeeder run successfuly");
+            System.out.println("*---------------------------*");
+            System.out.println("");
+            System.out.println("*----- ADMIN CREDENTIALS -----*");
             System.out.println("\tAdmin email: " + adminEmail);
             System.out.println("\tAdmin password " + adminPin);
+            System.out.println("");
+            System.out.println("*----- USER CREDENTIALS PATTERN-----*");
+            System.out.println("Each user follow the next pattern");
+            System.out.println("\tUser email: ${number}");
+            System.out.println("\tUser card number: ${number}");
+            System.out.println("\tUser password: ${number}");
+            System.out.println("The ${number} variable is defined in a set between \"1\" and \"" + size + "\"");
         } else {
             System.err.println("Seeder failed");
         }
