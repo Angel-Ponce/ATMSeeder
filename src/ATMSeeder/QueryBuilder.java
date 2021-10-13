82b890ef200242e313b4cd8a0aaa067110b2c4bb
Angel Ponce <acdpf000@gmail.com>
2021-10-12 23:20:17 -0600

    Changes on user insert method

--- src/ATMSeeder/QueryBuilder.java
+++ src/ATMSeeder/QueryBuilder.java
@@ -33,14 +33,18 @@ public class QueryBuilder {
         try {
             Connecter c = new Connecter();
             c.con = c.getConnection();
-            c.ps = c.con.prepareStatement("INSERT INTO \"admin\"(name,last_name,age,email,pin,last_access,pick) VALUES (?,?,?,?,?,?,?)");
-            c.ps.setString(1, user.getName());
-            c.ps.setString(2, user.getLastName());
-            c.ps.setInt(3, user.getAge());
-            c.ps.setString(4, user.getEmail());
-            c.ps.setInt(5, user.getPin());
-            c.ps.setTimestamp(6, Timestamp.valueOf(TIMESTAMP.format(user.getLastAccess())));
-            c.ps.setString(7, user.getPick());
+            c.ps = c.con.prepareStatement("INSERT INTO \"admin\"(card_number,name,last_name,age,email,pin,last_access,pick,current_balance,maximum_amount,count_pin_changed) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+            c.ps.setLong(1, user.getCardNumber());
+            c.ps.setString(2, user.getName());
+            c.ps.setString(3, user.getLastName());
+            c.ps.setInt(4, user.getAge());
+            c.ps.setString(5, user.getEmail());
+            c.ps.setInt(6, user.getPin());
+            c.ps.setTimestamp(7, Timestamp.valueOf(TIMESTAMP.format(user.getLastAccess())));
+            c.ps.setString(8, user.getPick());
+            c.ps.setInt(9, user.getCurrentBalance());
+            c.ps.setInt(10, user.getMaximumAmount());
+            c.ps.setInt(11, user.getCountPinChanged());
         } catch (Exception e) {
         }
     }
